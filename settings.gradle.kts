b2a20fa8396d43f9abd23199652ef571cce300ce
dwursteisen <david.wursteisen+git@gmail.com>
2021-04-25 12:53:48 +0200

    Add minigdx plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,18 @@
  */
 
 rootProject.name = "minigdx-game-template"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        maven {
+            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        }.mavenContent {
+            includeVersionByRegex("com.github.minigdx.(.*)", "(.*)", "LATEST-SNAPSHOT")
+            includeVersionByRegex("com.github.minigdx", "(.*)", "LATEST-SNAPSHOT")
+        }
+        mavenLocal()
+        jcenter()
+    }
+}
