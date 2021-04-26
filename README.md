db31622326c12ef8408c06c61c6d2f870bef3713
David Wursteisen <david.wursteisen+git@gmail.com>
2021-04-26 13:19:14 +0200

    Add comments and README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# minigdx-game-template
+# MiniGDX Game Template
+
+Create your first game using miniGDX by clicking the "Use this Template" button above.
+
+The game will be configured for:
+
+- the JVM platform (try it by running `./gradlew runJvm` command!)
+- the Web platform (try it by running `./gradlew runJs` command!)
+
+
+Get [more informations](https://github.com/minigdx) on miniGDX.
+
--- src/commonMain/kotlin/your.game/components/Cube.kt
+++ src/commonMain/kotlin/your.game/components/Cube.kt
@@ -1,8 +1,12 @@
 package your.game.components
 
+import com.github.dwursteisen.minigdx.Seconds
 import com.github.dwursteisen.minigdx.ecs.components.Component
 
 /**
- * Cube component: tag to mark entites that are cube.
+ * Cube component: tag to mark entities that are cube.
  */
-class Cube : Component
+class Cube(
+    // Remaining time of the cube movement
+    var duration: Seconds = 0f
+) : Component
